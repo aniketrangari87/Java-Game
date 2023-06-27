4d5618c40141963d0159913b2fc617389c9d701e
Aniket Rangari <aniketrangari66@gmail.com>
2023-06-27 22:16:47 +0530

    Guesser game Added

---
+++ gameLaunch.java
@@ -0,0 +1,103 @@
+
+import java.util.*;
+
+
+class Guesser{
+    int guessNum;
+
+    int guessingNumber(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Number from Guesser :");
+        guessNum = scan.nextInt();
+
+        return guessNum;
+    }
+}
+class Umpire {
+
+    int numFromGuesser;
+    int numFromPlayer1;
+    int numFromPlayer2;
+    int numFromPlayer3;
+
+    void collectNumFromGuesser(){
+
+        Guesser g = new Guesser();
+        numFromGuesser = g.guessingNumber();
+      
+
+    }
+    void collectNumFromPlayers(){
+        Player p1 = new Player();
+        Player p2 = new Player();
+        Player p3 = new Player();
+
+        numFromPlayer1 = p1.guessingNumber();
+        numFromPlayer2 = p2.guessingNumber();
+        numFromPlayer3 = p3.guessingNumber();
+       
+
+
+    }
+    void compare(){
+        if( numFromGuesser == numFromPlayer1){
+            if( numFromGuesser == numFromPlayer2 &&  numFromGuesser == numFromPlayer3){
+                System.out.println(" All wins the Game");
+            }
+            else if (  numFromGuesser == numFromPlayer2 ){
+                System.out.println(" Playe1 and 2 wins ");
+            }
+            else if(  numFromGuesser == numFromPlayer3){
+                System.out.println(  "Player 1 and 3 wins ");
+            }else{
+                System.out.println("Player 1 wins !!!");
+
+            }
+            
+        }
+        else if( numFromGuesser == numFromPlayer2){
+             if (  numFromGuesser == numFromPlayer3 ){
+                System.out.println(" Player 2  and 3  wins ");
+            }
+            else{
+                System.out.println("Player 2 wins !!!");
+            }
+        }
+        else if( numFromGuesser == numFromPlayer3){
+
+            if(numFromGuesser == numFromPlayer2){
+               System.out.println("Player 2 and 3 wins ");
+            }else{
+                  System.out.println("Player 3 wins !!!");
+            }
+          
+        }
+
+    }
+
+}
+
+class Player {
+    int guessNum;
+    int guessingNumber(){
+         Scanner scan = new Scanner(System.in);
+         System.out.println("Player ! Kindly Guess the number ");
+        guessNum = scan.nextInt();
+        return guessNum;
+
+    }
+}
+
+class gameLaunch{
+
+    
+
+    public static void main(String[] args) {
+        System.out.println("Game Started !!");
+        Umpire u = new Umpire();
+        u.collectNumFromGuesser();
+        u.collectNumFromPlayers();
+        u.compare();
+        
+    }
+}
